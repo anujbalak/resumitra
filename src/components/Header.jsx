9a4ffe13b9f4ea07321fb26d016af87ae58ac1c2
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-01-23 12:41:18 +0530

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(title) {
+    return (
+        <>
+            <header className="header">
+                <h1 className="title">
+                    {title}
+                </h1>
+            </header>
+        </>
+    )
+}
