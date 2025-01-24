adfdba88861770f35da4920753fbc2d757ea0783
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-01-24 17:53:35 +0530

    Load dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-
-import "./styles/homepage.css"
+import { useState } from "react"
 import './App.css'
+import "./styles/homepage.css"
+import "./styles/dashboard.css"
 import Homepage from './web/home/homepage.jsx'
+import Dashboard from "./web/dashboard/Dashboard.jsx";
 
 export default function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  function btnEventHandler() {
+    setCurrentPage('dashboard')
+  }
 
   return (
     <>
-    <Homepage />
+      {currentPage === 'home' && <RenderHome btnEventHandler={btnEventHandler} />}
+      {currentPage === 'dashboard' && <RenderDashboard />}
+      {/* <RenderDashboard currentPage={currentPage} /> */}
     </>
   )
 }
 
+function RenderHome({btnEventHandler}) {
+  return (
+    <div className="homepage-container">
+      <Homepage btnEventHandler={btnEventHandler}/>
+    </div>
+  )
+}
+
+function RenderDashboard() {
+  return (
+    <div className="dashboard-page">
+      <Dashboard />
+    </div>
+  );
+}
