d3f52260d2b3ab75b483f47f878eaea979c18b0f
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-01-24 18:05:18 +0530

    Fix props loading

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,9 @@
-export default function Header(title) {
+export default function Header({title}) {
     return (
-        <>
-            <header className="header">
-                <h1 className="title">
-                    {title}
-                </h1>
-            </header>
-        </>
+        <header className="header">
+            <h1 className="title">
+                {title}
+            </h1>
+        </header>
     )
 }
