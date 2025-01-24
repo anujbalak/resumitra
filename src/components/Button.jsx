1ff0961102a47e5d90e366cce659205b8d852443
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-01-24 17:54:19 +0530

    Add Event listener

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,21 @@
-export default function Button({name, url, target}) {
+export default function Button({name, url, target, btnEventHandler}) {
     return (
         <div className="button-container">
-            <ButtonContent name={name} url={url} target={target}/>
+            <ButtonContent name={name} url={url} target={target} btnEventHandler={btnEventHandler}/>
         </div>
     )
 }
 
-function ButtonContent({name, url, target}) {
+function ButtonContent({name, url, target, btnEventHandler}) {
     if(url) {
         return (
-            <button className="button" onClick={(e) => e.target.classList.toggle('clicked')}>
+            <button className="button" onClick={btnEventHandler}>
                 <a href={url} target={target} tabIndex={-5}>{name}</a>
             </button>
         )
     } else {
         return (
-            <button className="button">
+            <button className="button" onClick={btnEventHandler}>
                 {name}
             </button>
         )
