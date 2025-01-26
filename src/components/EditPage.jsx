e4eb1f31260735466591910b646d4d0654386fa8
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-01-26 19:24:10 +0530

    Create page for editor

---
+++ src/components/EditPage.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import EnterDetails from "./EnterDetails";
+import Header from "./header";
+import LeftSidebar from "./LeftSidebar";
+import ResumeScreen from "./ResumeScreen";
+import RightSidebar from "./RightSidebar";
+import Resume from "../module/resume.mjs";
+
+export default function EditPage({resumeDetails, backBtnHandler}) {
+    const [resumeData, setResumeData] = useState({});
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    let resume = new Resume();
+    if (isLoaded === false) {
+        let savedResume = JSON.parse(localStorage.getItem(resumeDetails.id));
+        if (savedResume !== null) {
+            resume = savedResume;
+            setResumeData(savedResume)
+            resume.name = savedResume.name
+            resume.id = savedResume.id
+        } else {
+            resume.name = resumeDetails.title;
+            resume.id = resumeDetails.id;
+            localStorage.setItem(resume.id, JSON.stringify(resume));
+        }
+        setIsLoaded(true);
+    }
+    console.log(resume);
+   
+    return (
+        <>
+        <Header title={resumeDetails.title}/>
+        <LeftSidebar backBtnHandler={backBtnHandler}/>
+        <EnterDetails />
+        <ResumeScreen />
+        <RightSidebar />
+        </>
+    )
+}
