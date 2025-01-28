a15353601d48349863d7f4ae725077ac0c2c1066
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-01-28 14:11:40 +0530

    Load Personal Detail section

--- src/components/EnterDetails.jsx
+++ src/components/EnterDetails.jsx
@@ -1,7 +1,13 @@
-export default function EnterDetails() {
+import PersonalDetails from "../web/editor/PersonalDetails";
+
+export default function EnterDetails({resumeData, setResumeData, resume}) {
     return (
         <div className="enter-details">
-            
+            <PersonalDetails
+                resumeData={resumeData} 
+                setResumeData={setResumeData}
+                resume={resume}
+            /> 
         </div>
     )
 }
