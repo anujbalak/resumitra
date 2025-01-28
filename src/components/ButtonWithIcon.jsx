76926ae19a0108308a62eb6c480e012779b408f4
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-01-28 19:10:26 +0530

    Show a flyout lable when hover button

--- src/components/ButtonWithIcon.jsx
+++ src/components/ButtonWithIcon.jsx
@@ -1,11 +1,38 @@
-export default function ButtonWithIcon({link, imgSource, btnHandler}) {
+import { useState } from "react"
+
+export default function ButtonWithIcon({label, link, imgSource, btnHandler}) {
+    const [isHover, setIsHover] = useState(false)
+    
+    function handlerMouseOver() {
+        setIsHover(true)
+    }
+    function handleMouseOut() {
+        setIsHover(false)
+    }
+    
     return (
-        <button className="btn-with-icon" onClick={btnHandler}>
-            {link !== undefined ? 
-            <a href={link}>
-                <img src={imgSource} alt=" " />
-            </a> : 
-            <img src={imgSource} alt=" " />}   
-        </button>
+        <div className="btn-container">
+            {isHover && <ShowLabel label={label} />}
+            <button
+                className="btn-with-icon"
+                onClick={btnHandler}
+                onMouseOver={handlerMouseOver}
+                onMouseOut={handleMouseOut}
+            >
+                {link !== undefined ?
+                <a href={link}>
+                    <img src={imgSource} alt=" " />
+                </a> :
+                <img src={imgSource} alt=" " />}
+            </button>
+        </ div>
+    )
+}
+
+function ShowLabel({label}) {
+    return (
+        <div className="label-flyout">
+            <p>{label}</p>
+        </div>
     )
 }
--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,18 +1,34 @@
 import ButtonWithIcon from "./ButtonWithIcon";
 import homeIcon from "/svg/home-angle.svg"
 import personalIcon from "/svg/personal.svg"
+import educationIcon from "/svg/education.svg"
+import projectIcon from "/svg/project-new.svg"
 
 export default function LeftSidebar({backBtnHandler}) {
     return (
         <div className="left-sidebar">
-            <ButtonWithIcon 
+            <ButtonWithIcon
+                label='Go to Dashboard'
                 imgSource={homeIcon}
                 btnHandler={backBtnHandler}
             />
-            <ButtonWithIcon 
-                link='#personal-details'
-                imgSource={personalIcon}
-            />
+            <div className="juper-links">
+                <ButtonWithIcon 
+                    label="Personal Details"
+                    link='#personal-details'
+                    imgSource={personalIcon}
+                />
+                <ButtonWithIcon
+                    label="Education Details"
+                    link="#education-details"
+                    imgSource={educationIcon}
+                />
+                <ButtonWithIcon
+                    label="Projects"
+                    link="#projects"
+                    imgSource={projectIcon}
+                />
+            </div>
         </div>
     )
 }
