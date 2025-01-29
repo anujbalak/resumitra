ec1625fb84bb67f327847bf498a53917a18de2a1
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-01-29 21:37:59 +0530

    Auto-Growing text area

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,4 +1,15 @@
 export default function Textarea({label, textareaHandler, value, className}) {
+
+    document.querySelectorAll("textarea").forEach(function(textarea) {
+        textarea.style.height = textarea.scrollHeight + "px";
+        textarea.style.overflowY = "hidden";
+      
+        textarea.addEventListener("input", function() {
+          this.style.height = "auto";
+          this.style.height = this.scrollHeight + "px";
+        });
+      })
+
     return (
         <div className="textarea-container">
             <span className={`text-area-text ${className}`}>{label}</span>
