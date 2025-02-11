f35fad14b361566a3790e3a2ed9d3a450a937e23
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-02-11 14:15:29 +0530

    Add new template and colors

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -3,7 +3,28 @@ import Preset from "./Preset.jsx";
 import collapseIcon from "/svg/collapse.svg"
 
 import preset1Img from "/templates/template_1.png"
-const colors = ['133, 0, 245', "30, 30, 30", "209, 69, 102", '13, 159, 201', '210, 194, 12', "2, 104, 0", "37, 40, 152"]
+import preset2Img from "/templates/template_2.png"
+
+const colors = [
+    '133, 0, 245', 
+    "30, 30, 30", 
+    "209, 69, 102", 
+    '13, 159, 201', 
+    '210, 194, 12', 
+    "2, 104, 0", 
+    "37, 40, 152", 
+    "102, 198, 161", 
+    "102, 51, 153", 
+    "214, 152, 46", 
+    "217, 74, 56", 
+    "156, 29, 147", 
+    "47, 47, 190", 
+    "234, 118, 203", 
+    "210, 15, 57",
+    "254, 100, 11",
+    "23, 146, 153",
+    "114, 135, 253",
+]
 
 export default function RightSidebar({ setShowRightSidebar, resumeData}) {
     function collapseBtnHandler() {
@@ -28,6 +49,15 @@ export default function RightSidebar({ setShowRightSidebar, resumeData}) {
                 break;
         }
     }
+
+    function changeAccentColor(e) {
+        document.body.style.setProperty('--accent-color', e.target.id)
+        const colors = document.querySelectorAll('span.color');
+        colors.forEach(color => {
+            color.classList.remove('selected');
+        })
+        e.target.classList.add('selected')
+    }
     return (
         <>
             <div className="right-sidebar">
@@ -45,7 +75,7 @@ export default function RightSidebar({ setShowRightSidebar, resumeData}) {
                             clickHandler={clickHandler}
                         />
                         <Preset
-                            img={preset1Img}
+                            img={preset2Img}
                             id="preset2"
                             clickHandler={clickHandler}
                         />
@@ -62,7 +92,7 @@ export default function RightSidebar({ setShowRightSidebar, resumeData}) {
                                     width: "1.4em", 
                                     height: "1.4em"
                                 }}
-                                onClick={(e) => document.body.style.setProperty('--accent-color', e.target.id)}
+                                onClick={changeAccentColor}
                             ></span>
                         )}
                     </div>
