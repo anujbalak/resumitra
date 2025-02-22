9966a1176a53613b234e6b703859a9783e6e6b35
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-02-22 13:11:15 +0530

    Show saved dates

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -7,6 +7,13 @@ import "react-day-picker/style.css";
 // type ValuePiece = Date | null;
 // type Value = ValuePiece | [ValuePiece, ValuePiece]
 export default function DateInput({label, islimit, valueHandler, defaultDate}) {
+    if (defaultDate) {
+        const dateInMMYYY = convertIntoDate(defaultDate)
+        defaultDate = new Date(dateInMMYYY.year, dateInMMYYY.month)
+    } else {
+        defaultDate = new Date();
+    }
+
     const [selected, setSelected] = useState(new Date());
     return (
         <div className="date-picker-container">
@@ -23,6 +30,7 @@ export default function DateInput({label, islimit, valueHandler, defaultDate}) {
                     startMonth={new Date(1950, 1)}
                     hidden
                     onMonthChange={valueHandler}
+                    defaultMonth={defaultDate}
                 />
             :
                 <DayPicker 
@@ -36,8 +44,63 @@ export default function DateInput({label, islimit, valueHandler, defaultDate}) {
                     startMonth={new Date(1950, 1)}
                     hidden
                     onMonthChange={valueHandler}
+                    defaultMonth={defaultDate}
                 />
             }
         </div>
     )
+}
+
+function convertIntoDate(date) {
+    const month = (date[0] + date[1] + date[2]).toLowerCase()
+    const monthCode = convertMonthIntoNumber(month)
+    const year = Number(date[4] + date[5] + date[6] + date[7])
+    const newDate = {year, month: monthCode}
+    return newDate
+}
+
+function convertMonthIntoNumber(month) {
+    let code = null
+    switch (month) {
+        case 'jan':
+            code = 1;           
+            break;
+        case 'feb':
+            code = 2;           
+            break;
+        case 'mar':
+            code = 3;           
+            break;
+        case 'apr':
+            code = 4;           
+            break;
+        case 'may':
+            code = 5;           
+            break;
+        case 'jun':
+            code = 6;           
+            break;
+        case 'jul':
+            code = 7;           
+            break;
+        case 'aug':
+            code = 8;           
+            break;
+        case 'sep':
+            code = 9;         
+            break;
+        case 'oct':
+            code = 10;           
+            break;
+        case 'nov':
+            code = 11;          
+            break;
+        case 'dec':
+            code = 12;            
+            break;
+        default:
+            code = 1
+            break;
+    }
+    return code;
 }
